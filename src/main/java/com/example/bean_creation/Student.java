dee074de37a929d88d0802aa3a7277f7ae07a768
puspendraJaiswal9 <pushpendrajaiswal988@gmail.com>
2025-04-03 12:08:48 +0530

    [Puspendra] Constructor Injection

--- src/main/java/com/example/bean_creation/Student.java
+++ src/main/java/com/example/bean_creation/Student.java
@@ -5,39 +5,10 @@ package com.example.bean_creation;
 public class Student {
     private String studentName;
     private int studentAge;
-    private StudentOne obj;
 
-    public String getStudentName() {
-        return studentName;
-    }
-
-    public void setStudentName(String studentName) {
-        this.studentName = studentName;
-    }
-
-    public int getStudentAge() {
-        return studentAge;
-    }
-
-    public void setStudentAge(int studentAge) {
-        this.studentAge = studentAge;
-    }
-
-    public StudentOne getObj() {
-        return obj;
-    }
-
-    public void setObj(StudentOne obj) {
-        this.obj = obj;
-    }
-
-    public Student(String studentName, int studentAge, StudentOne obj) {
+    public Student(String studentName, int studentAge) {
         this.studentName = studentName;
         this.studentAge = studentAge;
-        this.obj = obj;
-    }
-
-    public Student() {
     }
 
     @Override
@@ -45,7 +16,6 @@ public class Student {
         return "Student{" +
                 "studentName='" + studentName + '\'' +
                 ", studentAge=" + studentAge +
-                ", obj=" + obj +
                 '}';
     }
 }
