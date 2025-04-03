c77d799aa108048dbedbea75b6c5f4e13e1c0859
puspendraJaiswal9 <pushpendrajaiswal988@gmail.com>
2025-04-03 20:00:12 +0530

    [Puspendra]. Implement LifeCycle Method Using XML

--- src/main/java/com/example/bean_creation/Main.java
+++ src/main/java/com/example/bean_creation/Main.java
@@ -1,12 +1,13 @@
 package com.example.bean_creation;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String args[]){
-        ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
+        AbstractApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
         Student std=(Student) context.getBean("student");
         System.out.println(std);
+        context.registerShutdownHook();
     }
 }
--- src/main/java/com/example/bean_creation/Student.java
+++ src/main/java/com/example/bean_creation/Student.java
@@ -1,14 +1,31 @@
 package com.example.bean_creation;
 
-
-
 public class Student {
     private String studentName;
     private int studentAge;
 
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public int getStudentAge() {
+        return studentAge;
+    }
+
+    public void setStudentAge(int studentAge) {
+        this.studentAge = studentAge;
+    }
+
     public Student(String studentName, int studentAge) {
         this.studentName = studentName;
         this.studentAge = studentAge;
+    }
+
+    public Student() {
     }
 
     @Override
@@ -17,5 +34,13 @@ public class Student {
                 "studentName='" + studentName + '\'' +
                 ", studentAge=" + studentAge +
                 '}';
+    }
+
+    public void init(){
+        System.out.println("inside init method");
+    }
+
+    public void destroy(){
+        System.out.println("inside destroy method");
     }
 }
